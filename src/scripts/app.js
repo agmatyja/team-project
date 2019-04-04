67cb9f9410ec6a00079a96bd41b4d12c97a747d2
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-04-04 23:46:01 +0200

    WDP190404-10 - Change select menu to ul>li menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+const selectCategory = document.getElementById('selectCategory');
+selectCategory.addEventListener('click', function (event) {
+  event.stopPropagation();
+  const re = new RegExp('.*', 'g');
+  if (event.target.nodeName === 'LI') {
+    selectCategory.children[0].innerText = re.exec(event.target.innerText)[0];
+  }
+});
