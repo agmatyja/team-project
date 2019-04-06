d51ed338aed64f8716c347efbb24f70c5dde027f
Agnieszka Matyja <agmatyja@gmail.com>
2019-04-06 23:44:17 +0200

    WDP190404-18 carousel-products starts to work, swipe too

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,23 @@
-console.log('hello');
+import { tns } from './tiny-slider.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  tns({
+    items: 2,
+    controls: false,
+    responsive: {
+      '350': {
+        items: 3,
+        controls: true,
+        edgePadding: 30
+      },
+      '500': {
+        items: 4
+      }
+    },
+    container: '.slider-products',
+    swipeAngle: false,
+    speed: 400,
+    mouseDrag: true,
+    slideBy: 'page'
+  });
+});
