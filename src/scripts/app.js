a91afcf0cc850b1254b86179559f1612e241c3b8
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-04-09 22:35:51 +0200

    WDP190404-10 Fix categories and correct section name

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,7 @@
-console.log('hello');
-
 const selectCategory = document.getElementById('selectCategory');
 selectCategory.addEventListener('click', function (event) {
   event.stopPropagation();
-  const re = new RegExp('.*', 'g');
   if (event.target.nodeName === 'LI') {
-    selectCategory.children[0].innerText = re.exec(event.target.innerText)[0];
+    selectCategory.children[0].innerText = /.*/g.exec(event.target.innerText)[0];
   }
 });
