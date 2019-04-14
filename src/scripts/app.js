d3a60edcbb80f45eb139240d812afb7967ac8b4a
Agnieszka Matyja <agmatyja@gmail.com>
2019-04-14 10:27:50 +0200

    fiding carousel bootstrap and flickity

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,19 @@
+import Flickity from 'flickity';
+
 const selectCategory = document.getElementById('selectCategory');
 selectCategory.addEventListener('click', function (event) {
   event.stopPropagation();
   if (event.target.nodeName === 'LI') {
     selectCategory.children[0].innerText = /.*/g.exec(event.target.innerText)[0];
   }
+});
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  // eslint-disable-next-line
+  new Flickity('.carousel-promotion', {
+    cellAlign: 'left',
+    contain: true,
+    pageDots: true,
+    hash: true
+  });
 });
