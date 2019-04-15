636363db3e7027f7f892c9514860afecec4e8b08
Agnieszka Matyja <agmatyja@gmail.com>
2019-04-15 03:08:02 +0200

    WDP190404-22 left slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,6 +5,46 @@ document.addEventListener('DOMContentLoaded', function () {
     items: 1,
     responsive: {
       512: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      }
+    },
+    container: '.slider-promotion',
+    navContainer: '.dots > ul',
+    mouseDrag: true,
+    speed: 0,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3000,
+    autoplayText: ['▶', '❚❚']
+  });
+
+  /* tns({
+      items: 1,
+      container: '.slider-promotion-1',
+      swipeAngle: false,
+      speed: 400,
+      mouseDrag: true,
+      navContainer: '.dots > ul'
+    });
+    tns({
+      items: 1,
+      container: '.slider-promotion-2',
+      swipeAngle: false,
+      speed: 400,
+      mouseDrag: true,
+      navContainer: '.dots > ul'
+    }); */
+
+  tns({
+    items: 1,
+    responsive: {
+      512: {
         items: 2
       },
       768: {
