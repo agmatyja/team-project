51eaffa0ac96672f0e78862d5da7bfe27ca072a8
Mariusz Domanski <mariusz@domanski.pl>
2019-04-19 00:41:44 +0200

    Create thumb script

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -11,16 +11,41 @@ window.addEventListener('load', function () {
     clickedTab.classList.add('active');
     tabClickEvent.preventDefault();
 
-    const myContentPanes = document.getElementsByClassName('tab');
-    for (let i = 0; i < myContentPanes.length; i++) {
-      myContentPanes[i].classList.remove('active');
+    const myContentPanels = document.getElementsByClassName('gallery');
+    for (let i = 0; i < myContentPanels.length; i++) {
+      myContentPanels[i].classList.remove('active');
     }
     const anchorReference = tabClickEvent.target;
-    const activePaneId = anchorReference.getAttribute('href');
-    const activePane = document.querySelector(activePaneId);
-    activePane.classList.add('active');
+    const galleryactive = anchorReference.getAttribute('href');
+    const activePanel = document.querySelector(galleryactive);
+    activePanel.classList.add('active');
   }
   for (let i = 0; i < myTabs.length; i++) {
     myTabs[i].addEventListener('click', myTabClicks);
+  }
+});
+
+window.addEventListener('load', function () {
+  const thumbnails = document.getElementsByClassName('thumb');
+  function myTabClicks (tabClickEvent) {
+    for (let i = 0; i < thumbnails.length; i++) {
+      thumbnails[i].classList.remove('thumb-active');
+    }
+
+    const clickedThumb = tabClickEvent.currentTarget;
+    clickedThumb.classList.add('thumb-active');
+    tabClickEvent.preventDefault();
+
+    const bigPhoto = document.getElementsByClassName('bigPhoto');
+    for (let i = 0; i < bigPhoto.length; i++) {
+      bigPhoto[i].classList.remove('active-photo');
+    }
+    const anchorReference = tabClickEvent.target;
+    const bigPhotoactive = anchorReference.getAttribute('href');
+    const activePhoto = document.getElementsByClassName(bigPhotoactive);
+    activePhoto.classList.add('active-photo');
+  }
+  for (let i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener('click', myTabClicks);
   }
 });
