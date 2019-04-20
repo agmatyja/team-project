e436ecb796a218a141102c23815983abadf4b371
Mariusz Domanski <mariusz@domanski.pl>
2019-04-20 16:32:37 +0200

    Create slider thumbnail script

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -128,3 +128,57 @@ window.addEventListener('load', function () {
     thumbnails[i].addEventListener('click', myTabClicks);
   }
 });
+
+// thumbnails animation
+
+const thumbPrev = document.querySelector('.thumbnails .fa-chevron-left');
+const thumbNext = document.querySelector('.thumbnails .fa-chevron-right');
+const thumb = document.querySelector('.thumbnails-list .thumb');
+
+let thumbMoveLeft = function (items) {
+  for (let k = 0; k < items; k++) {
+    for (let i = 1; i < thumb.children.length; i++) {
+      if (
+        thumb.children[i - 1].classList.value !== 'active' &&
+        thumb.children[i].classList.value === 'active'
+      ) {
+        thumb.children[i - 1].classList.add('active');
+        for (let j = 1; j < thumb.children.length; j++) {
+          if (thumb.children[thumb.children.length - j].classList.value === 'active') {
+            thumb.children[thumb.children.length - j].classList.remove('active');
+            break;
+          }
+        }
+        break;
+      }
+    }
+  }
+};
+
+let thumbMoveRight = function (items) {
+  for (let k = 0; k < items; k++) {
+    for (let i = 0; i < thumb.children.length - 1; i++) {
+      if (
+        thumb.children[i].classList.value === 'active' &&
+        thumb.children[i + 1].classList.value !== 'active'
+      ) {
+        thumb.children[i + 1].classList.add('active');
+        for (let j = 0; j < thumb.children.length; j++) {
+          if (thumb.children[j].classList.value === 'active') {
+            thumb.children[j].classList.remove('active');
+            break;
+          }
+        }
+        break;
+      }
+    }
+  }
+};
+
+thumbPrev.addEventListener('click', function () {
+  thumbMoveLeft(6);
+});
+
+thumbNext.addEventListener('click', function () {
+  thumbMoveRight(6);
+});
