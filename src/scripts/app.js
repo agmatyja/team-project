496907eead159ff5ad01fd517089d7c701e46afd
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-04-23 17:05:48 +0200

    Modified js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,76 +1,154 @@
-'use strict';
-
-function toggleBed (visible) {
-  document.querySelector('#bed').classList.toggle('show', visible);
-  document.querySelector('#chair').classList.remove('show');
-  document.querySelector('#sofa').classList.remove('show');
-  document.querySelector('#table').classList.remove('show');
-  document.querySelector('#dining').classList.remove('show');
-}
-
-toggleBed();
-
-document.querySelector('#bed').addEventListener('click', function (e) {
-  e.preventDefault();
-  toggleBed();
+const selectCategory = document.getElementById('selectCategory');
+selectCategory.addEventListener('click', function (event) {
+  event.stopPropagation();
+  if (event.target.nodeName === 'LI') {
+    selectCategory.children[0].innerText = /.*/g.exec(event.target.innerText)[0];
+  }
 });
 
-function toggleChair (visible) {
-  document.querySelector('#chair').classList.toggle('show', visible);
-  document.querySelector('#bed').classList.remove('show');
-  document.querySelector('#sofa').classList.remove('show');
-  document.querySelector('#table').classList.remove('show');
-  document.querySelector('#dining').classList.remove('show');
+/// http://javascriptkit.com/javatutors/touchevents2.shtml
+function swipedetect (el, callback) {
+  let touchsurface = el;
+  let swipedir;
+  let startX;
+  let startY;
+  let distX;
+  let distY;
+  let threshold = 150;
+  // required min distance traveled to be considered swipe
+  let restraint = 100;
+  // maximum distance allowed at the same time in perpendicular direction
+  let allowedTime = 300;
+  // maximum time allowed to travel that distance
+  let elapsedTime;
+  let startTime;
+  let handleswipe = callback || function (swipedir) {};
+  touchsurface.addEventListener(
+    'touchstart',
+    function (e) {
+      let touchobj = e.changedTouches[0];
+      swipedir = 'none';
+      startX = touchobj.pageX;
+      startY = touchobj.pageY;
+      startTime = new Date().getTime(); // record time when finger first makes contact with surface
+      e.preventDefault();
+    },
+    { passive: false },
+    false
+  );
+
+  touchsurface.addEventListener(
+    'touchmove',
+    function (e) {
+      e.preventDefault(); // prevent scrolling when inside DIV
+    },
+    { passive: false },
+    false
+  );
+
+  touchsurface.addEventListener(
+    'touchend',
+    function (e) {
+      let touchobj = e.changedTouches[0];
+      distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+      distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+      elapsedTime = new Date().getTime() - startTime; // get time elapsed
+      if (elapsedTime <= allowedTime) {
+        // first condition for awipe met
+        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+          // 2nd condition for horizontal swipe met
+          swipedir = distX < 0 ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
+        } else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) {
+          // 2nd condition for vertical swipe met
+          swipedir = distY < 0 ? 'up' : 'down'; // if dist traveled is negative, it indicates up swipe
+        }
+      }
+      handleswipe(swipedir);
+      // e.preventDefault();
+    },
+    { passive: true },
+    false
+  );
 }
 
-toggleChair();
+document.addEventListener('DOMContentLoaded', function () {
+  const contentTabParent = document.querySelector(
+    '.section--products .panel-bar .menu'
+  );
+  const contentTab = document.querySelectorAll('.section--products .tab-content');
+  const tabs = document.querySelector('.section--products .nav-tabs .tab');
+  const tabsFragment = document.createDocumentFragment();
 
-document.querySelector('#chair').addEventListener('click', function (e) {
-  e.preventDefault();
-  toggleChair();
-});
+  for (let i = 0; i < contentTab.length; i++) {
+    const tabsElement = document.createElement('a');
+    /// tabsElement.setAttribute("href", "#");
+    tabsFragment.appendChild(tabsElement);
+  }
+  tabs.appendChild(tabsFragment);
 
-function toggleSofa (visible) {
-  document.querySelector('#sofa').classList.toggle('show', visible);
-  document.querySelector('#bed').classList.remove('show');
-  document.querySelector('#chair').classList.remove('show');
-  document.querySelector('#table').classList.remove('show');
-  document.querySelector('#dining').classList.remove('show');
-}
+  for (let i = 0; i < contentTab.length; i++) {
+    if (i === 0) {
+      const link = tabs.querySelectorAll('a');
+      contentTab[i].classList.add('show');
+      link[i].classList.add('active');
+    }
 
-toggleSofa();
+    tabs.children[i].addEventListener('click', function () {
+      for (let j = 0; j < contentTab.length; j++) {
+        const link = tabs.querySelectorAll('a');
+        if (i !== j) {
+          contentTab[j].classList.remove('show');
+          link[j].classList.remove('active');
+        } else {
+          contentTab[j].classList.add('show');
+          link[j].classList.add('active');
+        }
+      }
+    });
+  }
 
-document.querySelector('#sofa').addEventListener('click', function (e) {
-  e.preventDefault();
-  toggleSofa();
-});
+  let tabsLeft = function () {
+    for (let i = 0; i < contentTab.length; i++) {
+      const link = tabs.querySelectorAll('a');
+      if (contentTab[i].classList.contains('show')) {
+        contentTab[i].classList.remove('show');
+        link[i].classList.remove('active');
+        if (i !== 0) {
+          contentTab[i - 1].classList.add('show');
+          link[i - 1].classList.add('active');
+        } else {
+          contentTab[contentTab.length - 1].classList.add('show');
+          link[contentTab.length - 1].classList.add('active');
+        }
+        break;
+      }
+    }
+  };
 
-function toggleTable (visible) {
-  document.querySelector('#table').classList.toggle('show', visible);
-  document.querySelector('#bed').classList.remove('show');
-  document.querySelector('#chair').classList.remove('show');
-  document.querySelector('#sofa').classList.remove('show');
-  document.querySelector('#dining').classList.remove('show');
-}
+  let tabsRight = function () {
+    for (let i = 0; i < contentTab.length; i++) {
+      const link = tabs.querySelectorAll('a');
+      if (contentTab[i].classList.contains('show')) {
+        contentTab[i].classList.remove('show');
+        link[i].classList.remove('active');
+        if (i < contentTab.length - 1) {
+          contentTab[i + 1].classList.add('show');
+          link[i + 1].classList.add('active');
+        } else {
+          contentTab[0].classList.add('show');
+          link[0].classList.add('active');
+        }
+        break;
+      }
+    }
+  };
 
-toggleTable();
-
-document.querySelector('#table').addEventListener('click', function (e) {
-  e.preventDefault();
-  toggleTable();
-});
-
-function toggleDining (visible) {
-  document.querySelector('#dining').classList.toggle('show', visible);
-  document.querySelector('#bed').classList.remove('show');
-  document.querySelector('#chair').classList.remove('show');
-  document.querySelector('#table').classList.remove('show');
-  document.querySelector('#sofa').classList.remove('show');
-}
-
-toggleDining();
-
-document.querySelector('#dining').addEventListener('click', function (e) {
-  e.preventDefault();
-  toggleDining();
+  swipedetect(contentTabParent, function (swipedir) {
+    if (swipedir === 'left') {
+      tabsLeft();
+    }
+    if (swipedir === 'right') {
+      tabsRight();
+    }
+  });
 });
