14942d8dc2ed660461979f58852587c110e03a73
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-04-23 12:13:37 +0200

    Add tiny-slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,31 @@
-console.log('hello');
+import { tns } from './tiny-slider.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  tns({
+    items: 1,
+    responsive: {
+      512: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      1024: {
+        items: 4
+      }
+    },
+    container: '.slider-posts',
+    swipeAngle: false,
+    speed: 400,
+    mouseDrag: true,
+    navContainer: '.dots > ul'
+  });
+});
+
+const selectCategory = document.getElementById('selectCategory');
+selectCategory.addEventListener('click', function (event) {
+  event.stopPropagation();
+  if (event.target.nodeName === 'LI') {
+    selectCategory.children[0].innerText = /.*/g.exec(event.target.innerText)[0];
+  }
+});
