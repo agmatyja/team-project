6595f9df30221087f03990a4e83dbe80e8fe205a
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-04-24 23:08:45 +0200

    add tabs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,154 +1,72 @@
-const selectCategory = document.getElementById('selectCategory');
-selectCategory.addEventListener('click', function (event) {
-  event.stopPropagation();
-  if (event.target.nodeName === 'LI') {
-    selectCategory.children[0].innerText = /.*/g.exec(event.target.innerText)[0];
-  }
+let tab1 = document.querySelector('#bed');
+
+let tab2 = document.querySelector('#chair');
+
+let tab3 = document.querySelector('#sofa');
+
+let tab4 = document.querySelector('#table');
+
+let tab5 = document.querySelector('#dining');
+
+let tabs = document.querySelectorAll('.tab');
+
+let card = document.querySelectorAll('.card');
+
+let menu = document.querySelector('.menu');
+
+let first = document.querySelector('#first');
+let second = document.querySelector('#second');
+
+tab1.addEventListener('click', function () {
+  updateCard(first);
+  updateActiveStatus(tab1);
 });
 
-/// http://javascriptkit.com/javatutors/touchevents2.shtml
-function swipedetect (el, callback) {
-  let touchsurface = el;
-  let swipedir;
-  let startX;
-  let startY;
-  let distX;
-  let distY;
-  let threshold = 150;
-  // required min distance traveled to be considered swipe
-  let restraint = 100;
-  // maximum distance allowed at the same time in perpendicular direction
-  let allowedTime = 300;
-  // maximum time allowed to travel that distance
-  let elapsedTime;
-  let startTime;
-  let handleswipe = callback || function (swipedir) {};
-  touchsurface.addEventListener(
-    'touchstart',
-    function (e) {
-      let touchobj = e.changedTouches[0];
-      swipedir = 'none';
-      startX = touchobj.pageX;
-      startY = touchobj.pageY;
-      startTime = new Date().getTime(); // record time when finger first makes contact with surface
-      e.preventDefault();
-    },
-    { passive: false },
-    false
-  );
+tab2.addEventListener('click', function () {
+  updateCard(second);
+  updateActiveStatus(tab2);
+});
 
-  touchsurface.addEventListener(
-    'touchmove',
-    function (e) {
-      e.preventDefault(); // prevent scrolling when inside DIV
-    },
-    { passive: false },
-    false
-  );
+tab3.addEventListener('click', function () {
+  updateCard(first);
+  updateActiveStatus(tab3);
+});
 
-  touchsurface.addEventListener(
-    'touchend',
-    function (e) {
-      let touchobj = e.changedTouches[0];
-      distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
-      distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
-      elapsedTime = new Date().getTime() - startTime; // get time elapsed
-      if (elapsedTime <= allowedTime) {
-        // first condition for awipe met
-        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
-          // 2nd condition for horizontal swipe met
-          swipedir = distX < 0 ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
-        } else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) {
-          // 2nd condition for vertical swipe met
-          swipedir = distY < 0 ? 'up' : 'down'; // if dist traveled is negative, it indicates up swipe
-        }
-      }
-      handleswipe(swipedir);
-      // e.preventDefault();
-    },
-    { passive: true },
-    false
-  );
-}
+tab4.addEventListener('click', function () {
+  updateCard(second);
+  updateActiveStatus(tab4);
+});
 
-document.addEventListener('DOMContentLoaded', function () {
-  const contentTabParent = document.querySelector(
-    '.section--products .panel-bar .menu'
-  );
-  const contentTab = document.querySelectorAll('.section--products .tab-content');
-  const tabs = document.querySelector('.section--products .nav-tabs .tab');
-  const tabsFragment = document.createDocumentFragment();
+tab5.addEventListener('click', function () {
+  updateCard(first);
+  updateActiveStatus(tab5);
+});
 
-  for (let i = 0; i < contentTab.length; i++) {
-    const tabsElement = document.createElement('a');
-    /// tabsElement.setAttribute("href", "#");
-    tabsFragment.appendChild(tabsElement);
-  }
-  tabs.appendChild(tabsFragment);
-
-  for (let i = 0; i < contentTab.length; i++) {
-    if (i === 0) {
-      const link = tabs.querySelectorAll('a');
-      contentTab[i].classList.add('show');
-      link[i].classList.add('active');
-    }
-
-    tabs.children[i].addEventListener('click', function () {
-      for (let j = 0; j < contentTab.length; j++) {
-        const link = tabs.querySelectorAll('a');
-        if (i !== j) {
-          contentTab[j].classList.remove('show');
-          link[j].classList.remove('active');
-        } else {
-          contentTab[j].classList.add('show');
-          link[j].classList.add('active');
-        }
-      }
-    });
-  }
-
-  let tabsLeft = function () {
-    for (let i = 0; i < contentTab.length; i++) {
-      const link = tabs.querySelectorAll('a');
-      if (contentTab[i].classList.contains('show')) {
-        contentTab[i].classList.remove('show');
-        link[i].classList.remove('active');
-        if (i !== 0) {
-          contentTab[i - 1].classList.add('show');
-          link[i - 1].classList.add('active');
-        } else {
-          contentTab[contentTab.length - 1].classList.add('show');
-          link[contentTab.length - 1].classList.add('active');
-        }
-        break;
-      }
-    }
-  };
-
-  let tabsRight = function () {
-    for (let i = 0; i < contentTab.length; i++) {
-      const link = tabs.querySelectorAll('a');
-      if (contentTab[i].classList.contains('show')) {
-        contentTab[i].classList.remove('show');
-        link[i].classList.remove('active');
-        if (i < contentTab.length - 1) {
-          contentTab[i + 1].classList.add('show');
-          link[i + 1].classList.add('active');
-        } else {
-          contentTab[0].classList.add('show');
-          link[0].classList.add('active');
-        }
-        break;
-      }
-    }
-  };
-
-  swipedetect(contentTabParent, function (swipedir) {
-    if (swipedir === 'left') {
-      tabsLeft();
-    }
-    if (swipedir === 'right') {
-      tabsRight();
+function updateActiveStatus (activeItem) {
+  tabs.forEach(function (tabItem) {
+    if (tabItem !== activeItem) {
+      tabItem.classList.remove('active');
+      menu.classList.add('animation');
+    } else {
+      tabItem.classList.add('active');
+      menu.classList.add('animation');
     }
   });
-});
+}
+
+function updateCard (activeItem) {
+  card.forEach(function (tabItem) {
+    if (tabItem !== activeItem) {
+      tabItem.classList.add('temp');
+    } else {
+      tabItem.classList.remove('temp');
+    }
+  });
+}
+
+var tab = document.querySelectorAll('.tab');
+console.log(tab);
+
+for (var a = 0; a < tab.length; a++) {
+  console.log(tab[a].innerText);
+}
