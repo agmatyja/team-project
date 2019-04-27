94bc290867da331de3a3d0380f9038fa82bf8ebd
Mariusz Domanski <mariusz@domanski.pl>
2019-04-27 19:06:03 +0200

    Resolve conflicts

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -29,3 +29,57 @@ selectCategory.addEventListener('click', function (event) {
     selectCategory.children[0].innerText = /.*/g.exec(event.target.innerText)[0];
   }
 });
+
+// thumbnails animation
+
+const thumbPrev = document.querySelector('.thumbnails .fa-chevron-left');
+const thumbNext = document.querySelector('.thumbnails .fa-chevron-right');
+const thumb = document.querySelector('.thumbnails-list .thumb');
+
+let thumbMoveLeft = function (items) {
+  for (let k = 0; k < items; k++) {
+    for (let i = 1; i < thumb.children.length; i++) {
+      if (
+        thumb.children[i - 1].classList.value !== 'active' &&
+        thumb.children[i].classList.value === 'active'
+      ) {
+        thumb.children[i - 1].classList.add('active');
+        for (let j = 1; j < thumb.children.length; j++) {
+          if (thumb.children[thumb.children.length - j].classList.value === 'active') {
+            thumb.children[thumb.children.length - j].classList.remove('active');
+            break;
+          }
+        }
+        break;
+      }
+    }
+  }
+};
+
+let thumbMoveRight = function (items) {
+  for (let k = 0; k < items; k++) {
+    for (let i = 0; i < thumb.children.length - 1; i++) {
+      if (
+        thumb.children[i].classList.value === 'active' &&
+        thumb.children[i + 1].classList.value !== 'active'
+      ) {
+        thumb.children[i + 1].classList.add('active');
+        for (let j = 0; j < thumb.children.length; j++) {
+          if (thumb.children[j].classList.value === 'active') {
+            thumb.children[j].classList.remove('active');
+            break;
+          }
+        }
+        break;
+      }
+    }
+  }
+};
+
+thumbPrev.addEventListener('click', function () {
+  thumbMoveLeft(6);
+});
+
+thumbNext.addEventListener('click', function () {
+  thumbMoveRight(6);
+});
